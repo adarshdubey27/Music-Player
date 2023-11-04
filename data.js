0552495158f200809680f3082a1e11fc64a54586
Adarsh Dubey <141320327+adarshdubey27@users.noreply.github.com>
2023-11-04 20:29:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'vishnu strotram',
+        path: 'musics/song 1.mp3',
+        artist: ' G. Gayathri Devi',
+        cover: 'images/cover 1.jpeg'
+    },
+    {
+        name: 'Hanuman Chalisa',
+        path: 'musics/Song 2.mp3',
+        artist: 'Gulshan Kumar',
+        cover: 'images/cover 2.jpg'
+    },
+    {
+        name: 'One Love',
+        path: 'musics/Song 3.mp3',
+        artist: 'Shubh',
+        cover: 'images/cover 3.jpg'
+    },
+    {
+        name: 'jai Jai Shree Ram Bolega',
+        path: 'musics/Song 4.mp3',
+        artist: 'HINDU',
+        cover: 'images/cover 4.jpg'
+    },
+    {
+        name: 'Babam Bam',
+        path: 'musics/Song 5.mp3',
+        artist: 'Paradox',
+        cover: 'images/cover 5.png'
+    },
+];
